a77e4c88842a6fd264ba74116d8816a16d627a00
Mark Teasdale <mteasdal@btinternet.com>
2021-03-26 14:03:18 +0000

    Added test for files in process directory

--- scp/scp.go
+++ scp/scp.go
@@ -13,3 +13,27 @@ func directoryCheck(directory string) (bool, error) {
 
 	return true, nil
 }
+
+//getFileUsage returns the files to process from a directory
+func getFileUsage(directory string) ([]string, error) {
+	var filesList []string
+
+	f, err := os.Open(directory)
+
+	if err != nil {
+		return filesList, err
+	}
+
+	defer f.Close()
+
+	fileInfo, err := f.ReadDir(-1)
+	if err != nil {
+		return filesList, err
+	}
+
+	for _, file := range fileInfo {
+		filesList = append(filesList, file.Name())
+	}
+
+	return filesList, nil
+}
--- scp/scp_test.go
+++ scp/scp_test.go
@@ -16,3 +16,26 @@ func TestDirectoryCheckTrue(t *testing.T) {
 
 	assert.True(t, expected, actual)
 }
+
+//TestDirectoryCheckFalse test directoryCheck returns false for
+//a non existent directory
+func TestDirectoryCheckFalse(t *testing.T) {
+	directory := "../scpfalse/"
+	expected := false
+
+	actual, _ := directoryCheck(directory)
+
+	assert.False(t, expected, actual)
+
+}
+
+//TestGetUsageFiles checks that getUsageFiles returns
+//Files to process
+func TestGetUsageFiles(t *testing.T) {
+	directory := "../test_data/"
+	expected := 3
+
+	actual, _ := getFileUsage(directory)
+
+	assert.Equal(t, expected, len(actual))
+}
